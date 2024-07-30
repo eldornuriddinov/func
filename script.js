cd4265fcf9db2f91349d166bf346385a664d37b0
eldornuriddinov <nuriddinoveldor133@gmail.com>
2024-07-30 16:49:25 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,99 @@
+//  for (let i = 0; i < 10; i++) {
+//     console.log('hello');
+    
+//  }
+
+
+
+
+
+// let i = 0
+// while(i < 5){
+//    console.log(i);
+//    i++
+// }
+
+
+
+// let i = 0
+// do{
+//    console.log(i);
+//    i++
+// }  while(i < 10)
+
+
+
+
+
+
+
+
+
+
+
+// function sayHello(name){
+//    console.log('Heil' + name);
+// }
+
+// sayHello('Hitler')
+
+
+
+
+// decloration
+//  function sum(a, b) {
+//    console.log(a + b);
+   
+// }
+
+// sum(1900, 45)
+
+// sum( 'Hi', 'Nigga')
+
+// sum('Adolf', 228)
+
+// sum('')
+
+
+//  expression 
+//  const greet = function(name){
+//     console.log('qalesan ' + name);
+//  }
+//  greet('Otabek')
+
+
+// const hi = (a, b)=> a + b
+
+// console.log(hi(5, 5));
+
+
+
+// const plus = (a, b) => a + b
+// console.log(plus(10, 10));
+
+// const minus = (a, b) => a - b
+// console.log(minus(20, 10));
+
+// const multiply = (a, b) => a * b
+// console.log(multiply(10, 10));
+
+// const devide = (a, b) => a / b
+// console.log(devide(10, 5));
+
+
+
+
+
+// const me = function(a, b, c){
+//    console.log('Привет, ' + 'меня ' + 'зовут ' + a , b  + ' мне ' + c + ' лет' );
+// }
+
+
+
+
+const me = function(name, surname, age){
+   console.log(`Привет меня зовут ${name} ${surname}, мне ${age} лет`);       
+}
+
+me('Eldor', 'Nuriddinov',  16  )
+
